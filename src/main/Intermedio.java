7d4f4f527c79c99d214c6007bf2ce25660040745
SpeedChela <mario.pe.hernandez@hotmail.com>
2025-06-02 19:33:58 -0600

    Proyecto de Automatas, la base

---
+++ src/main/Intermedio.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author speedchela
+ */
+public class Intermedio {
+
+/*
+ Base del proyecto
+ Se deben de realizar varios pasos para que se realize bien el codigo intermedio:
+    1.- Desglose de operaciones por variables temporales (t1,t2,t3,etc)
+    2.- Crear las etiquetas en cuestion de las condicionales los cuales son
+         if -> si
+         if else -> si sino
+         while -> mentre
+         and -> y
+         or -> o
+         not -> non
+        
+    Estas deben de tener sus etiquetas las cuales cumplan el proposito a manera de ifs
+    */    
+}
